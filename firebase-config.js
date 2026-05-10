cbe8b5aa2dabf43f1e82b0782de9eba4f7f5e6e7
moscowtest <slopplay4321@gmail.com>
2026-05-10 16:34:11 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+(function(){
+  const _0x1f3e = {
+    _0x2b9c: "AIzaSyCuOIt7wMUrN8vu9RQqivNj9w3CdFitrqE",
+    _0x277b: "moscow-live-tests.firebaseapp.com",
+    _0x1f9c: "moscow-live-tests",
+    _0x4e3b: "moscow-live-tests.firebasestorage.app",
+    _0x5a8c: "956332293918",
+    _0x3a2f: "1:956332293918:web:e7f66986cca2a30474130f"
+  };
+  
+  window._0x2c3a = _0x1f3e;
+})();
